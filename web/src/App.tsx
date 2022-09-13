d5967d3ea6beadfcdb3ec6c7152140f0d7140dec
Lucas Kroger <lucasmkroger12@gmail.com>
2022-09-13 12:22:29 -0300

    nlw

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,10 @@
-interface ButtonProps {
-  title: string;
-}
+import './styles/main.css'
 
-function Button(props: ButtonProps) {
-  return (
-    <button>{props.title}</button>
-  )
-}
 
 
 function App() {
   return (
-    <>
-    <Button title="Send 1"/>
-    <Button title="Send 2"/>
-    <Button title="Send 3"/>
-    <Button title="Hello World"/>
-    </>
+    <div className="w-8 h-8 bg-violet-500"></div>
   )
 }
 
