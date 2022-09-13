9adfd52164a389a92688cd82f5127c84ca7ebf81
Lucas Kroger <lucasmkroger12@gmail.com>
2022-09-13 16:38:44 -0300

    atu NLW

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,13 +2,18 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
-    './index.html'
+    './index.html',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
-        background: "url('/Fundo.png')"
-      }
+        galaxy: "url('/Fundo.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.88%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.88%)',
+      },
     },
   },
   plugins: [],
